bf22e68978e44e606f09adde4c03e959fac7ec8b
Gabe-LDTG <141433328+Gabe-LDTG@users.noreply.github.com>
2023-10-19 21:06:32 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,4 +2,12 @@
     $user = "LAPTOP-H6VM1SGS\Brisk"; 
     $password = "";
     $ODBCConnection = odbc_connect("DRIVER={Devart ODBC Driver for SQL Server};Server=myserver;Database=mydatabase; Port=myport;String Types=Unicode", $user, $password);
+
+    $SQLQuery = "SELECT * FROM customers";
+    $RecordSet = odbc_exec($ODBCConnection, $SQLQuery);
+
+    while (odbc_fetch_row($RecordSet)) {
+        $result = odbc_result_all($RecordSet, "border=1");
+    }
+    odbc_close($ODBCConnection);
 ?>
